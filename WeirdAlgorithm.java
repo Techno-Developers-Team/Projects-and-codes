d0b3172df4ae7153528eb339e00720c56f82eb60
TechnoLord-epg <96922322+TechnoLord-epg@users.noreply.github.com>
2025-10-19 01:25:06 +0530

    Create WeirdAlgorithm.java

---
+++ WeirdAlgorithm.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class WeirdAlgorithm{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		while(n!=1){
+			System.out.println(n+" ");
+			if(n%2==0){
+				n=n/2;
+			}
+			else{
+				n=(n*3)+1;
+			}
+		}
+		System.out.println(1);
+	}
+}
